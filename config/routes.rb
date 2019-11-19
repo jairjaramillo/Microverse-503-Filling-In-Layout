c1bd32c9f1641877a9536513a2c93af3750e5ebe
Jair Jaramillo <jair.jaramillo@outlook.com>
2019-11-19 15:11:45 -0600

    test

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'site#home'
   get 'site/home'
   get 'site/about'
   get 'site/help'
   get 'site/log_in'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
